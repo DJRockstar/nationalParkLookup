8b8e55ac4a7dbaec46bc471d5522c925f811ad64
DJRockstar <digvijays18@gmail.com>
2019-04-17 21:02:41 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const searchURL = "https://developer.nps.gov/api/v1/parks";
+
+function formatQueryParams(params){
+    const queryItems = Object.keys(params)
+    .map(key =>`${key}=${params[key]}`)
+    return queryItems.join('&');
+}
+
+function displayResults(responseJson){
+    $("#results").empty();
+    for(let i = 0; i < $("#js-max-results").val(); i++){
+        $("#results").append(`
+         <ul id="results-list">
+            <li class="park-name"><strong>${responseJson.data[i].fullName}</strong></li>
+            <li class="description"><strong>Description:</strong> ${responseJson.data[i].description}</li>
+            <li class="park-link"><a href="${responseJson.data[i].url}">Link to Park</a></li>
+        </ul>
+        `) 
+        $("#results").removeClass("hidden");
+    }
+  
+}
+
+function getInfo(stateCode, maxResults=10){
+    const params = {
+        api_key : "xGeAedOFFc4z4HHevvfvFId8YSXlzVUCg7VtwhrJ",
+        stateCode: stateCode,
+        maxResults
+    };
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString;
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(responseJson => { 
+        console.log(responseJson);
+        displayResults(responseJson);
+    }) 
+}
+
+
+function watchForm(){
+    $("form").on("submit", (e)=>{
+        e.preventDefault();
+        const stateCode = $("select").val();
+        const maxResults = $("#js-max-results").val();
+        getInfo(stateCode, maxResults);
+    })
+}
+
+$(watchForm)
